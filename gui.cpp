eebed97eb9914356ce944dac97e1cf26b1367d2c
lfs <lfs>
2025-09-30 16:28:28 +0200

    ImGui demo window

--- gui.cpp
+++ gui.cpp
@@ -1,52 +1,93 @@
 #include "gui.h"
 
-GUI::GUI(){
+GUI::GUI()
+{
 
     SDL_Init(SDL_INIT_VIDEO);
-    
-    m_window = SDL_CreateWindow("TodoList", 640, 380, 0);
 
-    if( m_window == NULL){
+    m_window = SDL_CreateWindow("TodoList", 640*2, 380*2, 0);
+    
+    if (m_window == NULL)
+    {
         std::cerr << "Error opening window";
     }
 
     m_renderer = SDL_CreateRenderer(m_window, NULL);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+    // Setup scaling
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(
+        main_scale); // Bake a fixed style scale. (until we have a
+                     // solution for dynamic style scaling, changing this
+                     // requires resetting Style + calling this again)
+                     // makes this unnecessary. We leave both here for
+                     // documentation purpose)
+    style.FontScaleDpi = main_scale;
+
+    style.FontSizeBase = 20.f;
+    io.Fonts->AddFontDefault();
+
+    ImGui_ImplSDL3_InitForSDLRenderer(m_window, m_renderer);
+    ImGui_ImplSDLRenderer3_Init(m_renderer);
 }
 
-bool GUI::runGUI(){
+bool GUI::runGUI()
+{
 
-
-    bool appRunning {true};
+    bool appRunning{true};
 
     SDL_Event event{};
     static int tick{1699};
 
     SDL_ShowWindow(m_window);
-    while( appRunning){
+    while (appRunning)
+    {
         SDL_PollEvent(&event);
+        ImGui_ImplSDL3_ProcessEvent(&event);
 
         switch (event.type)
         {
-        case SDL_EVENT_QUIT :
+        case SDL_EVENT_QUIT:
             appRunning = false;
             return quitGUI();
             break;
-        
+
         default:
             break;
+        
         }
-       
-        SDL_SetRenderDrawColor(m_renderer, 3*tick%255, 7*tick%255, 11*tick%255,  0xff);
-        SDL_RenderClear(m_renderer);
-        SDL_RenderPresent(m_renderer);
 
+
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+
+
+        SDL_SetRenderDrawColor(m_renderer, 3 * tick % 255, 7 * tick % 255, 11 * tick % 255, 0xff);
+        SDL_RenderClear(m_renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), m_renderer);
+        SDL_RenderPresent(m_renderer);
     }
-    
 
     return false;
 }
 
-bool GUI::quitGUI(){
+bool GUI::quitGUI()
+{
 
     SDL_DestroyWindow(m_window);
     SDL_Quit();
--- gui.h
+++ gui.h
@@ -4,6 +4,11 @@
 
 #include <SDL3/SDL.h>
 
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+
+
 class GUI
 {
 private:
